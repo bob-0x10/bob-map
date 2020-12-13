94221c3f8aeca7d65b35a578cc560d09f99fd96b
Lee Kyung-ha <i_am@nulleekh.com>
2020-12-13 12:23:52 +0900

    [FIX] Modify setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,34 @@
+create table if not exists SPRING_SESSION
+(
+	PRIMARY_ID char(36) not null
+		primary key,
+	SESSION_ID char(36) not null,
+	CREATION_TIME bigint not null,
+	LAST_ACCESS_TIME bigint not null,
+	MAX_INACTIVE_INTERVAL int not null,
+	EXPIRY_TIME bigint not null,
+	PRINCIPAL_NAME varchar(100) null,
+	constraint SPRING_SESSION_IX1
+		unique (SESSION_ID)
+);
+
+create index SPRING_SESSION_IX2
+	on SPRING_SESSION (EXPIRY_TIME);
+
+create index SPRING_SESSION_IX3
+	on SPRING_SESSION (PRINCIPAL_NAME);
+
+create table if not exists SPRING_SESSION_ATTRIBUTES
+(
+	SESSION_PRIMARY_ID char(36) not null,
+	ATTRIBUTE_NAME varchar(200) not null,
+	ATTRIBUTE_BYTES blob not null,
+	primary key (SESSION_PRIMARY_ID, ATTRIBUTE_NAME),
+	constraint SPRING_SESSION_ATTRIBUTES_FK
+		foreign key (SESSION_PRIMARY_ID) references SPRING_SESSION (PRIMARY_ID)
+			on delete cascade
+);
+
 create table if not exists node
 (
 	node_id int not null
@@ -59,8 +90,8 @@ create table if not exists log
 (
 	`index` int auto_increment
 		primary key,
-	is_processed tinyint(1) default 0 not null,
-	pos_processed tinyint(1) default 0 null,
+	att_processed tinyint(1) default 0 not null,
+	pos_processed tinyint(1) default 0 not null,
 	node_id int not null,
 	device_id int not null,
 	RSSI tinyint not null,
